ae02d4fa8f58894838f64299035890e20173fc89
Martien van den Akker <martien.van.den.akker@oracle.com>
2025-04-17 09:54:45 +0200

    Add folders to cleans

--- scripts/bin/ccleaner.sh
+++ scripts/bin/ccleaner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #############################################################################
 # Cleanup caches and temp files of several applications.
-# Initially VS Code
+# For VS Code, see: https://bobbyhadz.com/blog/vscode-clear-cache
+#                   https://builtin.com/articles/vs-code-clear-cache
 #
 # @author Martien van den Akker, Oracle Consulting
 # @version 1.0, 2025-04-17 - Initial creation
@@ -9,14 +10,31 @@
 #############################################################################
 SCRIPTPATH=$(dirname $0)
 #
-export VSCODE_CONFIG=~/.config/Code
-export VSCODE_CACHE=$VSCODE_CONFIG/Cache
-export VSCODE_CACHED_DATA=$VSCODE_CONFIG/CachedData
+export VSCODE_CONFIG="$HOME/.config/Code"
+export VSCODE_CACHE="$VSCODE_CONFIG/Cache"
+export VSCODE_CODE_CACHE="$VSCODE_CONFIG/Code Cache"
+export VSCODE_CACHED_DATA="$VSCODE_CONFIG/CachedData"
+export VSCODE_LOCAL_STG="$VSCODE_CONFIG/Local Storage"
+export VSCODE_SESSION_STG="$VSCODE_CONFIG/Session Storage"
+export VSCODE_CACHED_PRFL_DATA="$VSCODE_CONFIG/CachedProfilesData/"
+export VSCODE_USER="$VSCODE_CONFIG/User"
+export VSCODE_USER_GLOB_STG="$VSCODE_USER/globalStorage"
+export VSCODE_USER_HISTORY="$VSCODE_USER/History"
+export VSCODE_USER_WORKSPACE_STG="$VSCODE_USER/workspaceStorage"
+
+
 #
 function cleanup_vscode () {
   echo "Cleanup VS Code caches in $VSCODE_CONFIG"
-  rm -rf $VSCODE_CACHE/*
-  rm -rf $VSCODE_CACHED_DATA/*
+  rm -rf "$VSCODE_CACHE/*"
+  rm -rf "$VSCODE_CACHED_DATA/*"
+  rm -rf "$VSCODE_CODE_CACHE/*"
+  rm -rf "$VSCODE_LOCAL_STG/*"
+  rm -rf "$VSCODE_SESSION_STG/*"
+  rm -rf "$VSCODE_CACHED_PRFL_DATA/*" 
+  rm -rf "$VSCODE_USER_GLOB_STG/*" 
+  rm -rf "$VSCODE_USER_HISTORY/*" 
+  rm -rf "$VSCODE_USER_WORKSPACE_STG/*" 
 }
 #
 function main () {
